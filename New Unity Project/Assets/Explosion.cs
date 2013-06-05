d6bb8f99b11ee2d0f9f55ffdd5859bddbb52455c
Alex Pharo <apharo@texhorseman.com>
2013-06-04 20:19:16 -0500

    Trying to add new files

---
+++ New Unity Project/Assets/Explosion.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Dynamics.Contacts;
+using FVector2 = Microsoft.Xna.Framework.FVector2;
+using FarseerPhysics.Collision.Shapes;
+
+
+public class Explosion : MonoBehaviour {
+	
+	public float minRadius = 1.0f;
+	public float maxRadius = 10.0f;
+	public float expansionTime = 0.2f;
+	public float lifeSpan = 1.0f;
+	public float damage = 100.0f;
+	public float maxImpactOnPlayer = 20.0f;
+	public float minImpactOnPlayer = 2.0f;
+	
+	private Body body;
+	private Shape shape;
+	private float expansionRate;
+	
+	// Use this for initialization
+	void Start () {
+		SetupPhysics();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		shape.Radius += expansionRate*Time.fixedDeltaTime;
+	}
+	
+	private void SetupPhysics() {
+		body = GetComponent<FSBodyComponent>().PhysicsBody;
+		body.IsSensor = true;
+		body.GravityScale = 0.0f; //Explosions aren't affected by gravity
+		
+		shape = GetComponent<FSShapeComponent>().GetShape();
+		shape.Radius = minRadius;
+		
+		expansionRate = (maxRadius - minRadius) / expansionTime; 
+	}
+
+	private bool OnCollisionEvent(Fixture fixtureA, Fixture fixtureB, Contact contact) {
+		return false;
+	}
+}
