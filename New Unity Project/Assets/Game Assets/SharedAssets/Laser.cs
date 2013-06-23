63cddeef67ba2c38d4d985f02352a2a3d3bd70e7
apharo <apharo@texhorseman.com>
2013-06-22 19:59:32 -0500

    Lasers added

---
+++ New Unity Project/Assets/Game Assets/SharedAssets/Laser.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using System.Collections;
+using FarseerPhysics.Common;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Dynamics.Contacts;
+using FarseerPhysics.Collision.Shapes;
+using FVector2 = Microsoft.Xna.Framework.FVector2;
+
+//this script should be attached to a GameObject that has a body attached but no fixtures.
+//Lasers should be kinematic objects!!!!
+
+public class Laser : MonoBehaviour {
+	
+	public static int edgeCount = 5;
+	
+	public float warmupTime = 0.0f;
+	public float lifeSpan = 2.0f;
+	public float width = 0.25f;
+	private float length = 50.0f;
+	
+	//Stuff needed for rotation and movement of the laser
+	public GameObject host;
+	public bool followHost = true;
+	public float offsetRadius = 0.0f;
+	
+	private Body body;
+	
+	
+	// Use this for initialization
+	void Start () {
+		//yield return StartCoroutine(FadeIn(warmupTime));
+		SetupPhysics();
+		StartCoroutine(Sweep (90.0f, 2.0f));
+		StartCoroutine(SetupLifeSpan(lifeSpan));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	/// <summary>
+	/// Sweep the laser with specified degreesToSweep and sweepTime.
+	/// </summary>
+	/// <param name='degreesToSweep'>
+	/// Degrees to sweep.
+	/// </param>
+	/// <param name='sweepTime'>
+	/// Sweep time.
+	/// </param>
+	public IEnumerator Sweep(float degreesToSweep, float sweepTime) {
+		float angularVelocityInRadians = degreesToSweep / 180.0f * Mathf.PI / sweepTime;
+		body.AngularVelocity = angularVelocityInRadians;
+		
+		yield return new WaitForSeconds(sweepTime);
+		
+		body.AngularVelocity = 0.0f;
+	}
+	
+	public void SetAngle(float angleInDegrees) {
+		body.Rotation = angleInDegrees / 180.0f * Mathf.PI;	
+	}
+	
+	private IEnumerator FadeIn(float waitTime) {
+		yield return new WaitForSeconds(waitTime);
+	}
+	
+	/// <summary>
+	/// Setups the life span. Destroys itself when it the timer runs out the laser destroys itself. Later should call a FadeOut sequence rather than destruction
+	/// </summary>
+	/// <param name='lifeSpan'>
+	/// Life span.
+	/// </param>
+	private IEnumerator SetupLifeSpan(float lifeSpan) {
+		yield return new WaitForSeconds(lifeSpan);
+		GameObject.Destroy(gameObject);
+	}
+	
+	/// <summary>
+	/// Setups the physics. The default directionf of this laser is pointing straight down!! 0 degrees is pointing straight down!!
+	/// </summary>
+	private void SetupPhysics() {
+		if(body == null) {
+			body = GetComponent<FSBodyComponent>().PhysicsBody;
+			Vertices laserPoints = PolygonTools.CreateCapsule(length, width, edgeCount);
+			laserPoints.Translate(new FVector2(0.0f, -(offsetRadius + length/2.0f)));
+			body.CreateFixture(new PolygonShape(laserPoints, 1.0f));		
+		}
+	}
+	
+}
