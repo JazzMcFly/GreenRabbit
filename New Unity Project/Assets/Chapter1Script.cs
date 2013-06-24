752af7058659077da731252adddedc5290911737
Alex Pharo <apharo@texhorseman.com>
2013-06-23 21:33:36 -0500

    Added first wave

--- New Unity Project/Assets/Chapter1Script.cs
+++ New Unity Project/Assets/Chapter1Script.cs
@@ -10,10 +10,12 @@ public class Chapter1Script : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-		Scheduler enemy0 = Scheduler.SpawnEnemy(EnemyBasic, new FVector2(0.0f, 10.0f));
-		enemy0.AddMovementEvent(new FVector2(3.0f, 2.0f), 2.0f);
-		enemy0.AddMovementEvent(new FVector2(-4.0f, 10.0f), 1.0f, 1, 2.5f, true); 
-		enemy0.AddFireEvent(0.5f, 2, 2.0f);
+		for(int i = 0; i < 5; i++) {
+			Scheduler enemy0 = Scheduler.SpawnEnemy(EnemyBasic, new FVector2((float)(2*i - 5.0f), 10.0f));
+			enemy0.AddMovementEvent(new FVector2((float) (2*i) - 4.0f, 4.0f), 2.0f, 0, i*0.5f);
+			enemy0.AddMovementEvent(new FVector2(-8.0f, 12.0f), 1.0f, 1, 2.5f, true); 
+			enemy0.AddFireEvent(0.5f, 2, 2.0f + i*0.5f);
+		}
 	}
 	
 	// Update is called once per frame
